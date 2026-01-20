b6f4b2867b52e2a0f4d233f9c22273233139ab37
ShakebShamsi <shakebshamsi@hotmail.com>
2026-01-20 10:38:21 +0530

    UserLoggedIn: Footer component added

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,10 +1,12 @@
 import { AppHeader } from "@/components/app-header";
+import { LandingFooter } from "@/components/landing/landing-footer";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen flex flex-col">
-      <AppHeader />
-      <main className="flex-1">{children}</main>
-    </div>
-  );
+   return (
+      <div className="min-h-screen flex flex-col">
+         <AppHeader />
+         <main className="flex-1">{children}</main>
+         < LandingFooter />
+      </div>
+   );
 }
